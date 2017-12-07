46b203f58784f7074e5699f0e66f84ec3ac46152
weijj-qhd <weijunjie@madecare.com>
2017-12-07 13:52:12 +0800

    修改15

--- src/com/weijj/test/Weijj.java
+++ src/com/weijj/test/Weijj.java
@@ -4,7 +4,7 @@ package com.weijj.test;
 
 public class Weijj {
 	public static void main(String[] args) {
-		System.out.println("diertiaodev11");
+		System.out.println("diertiaodev15");
 		System.out.println("diyitiaodeal4");
 		System.out.println("diyitiaoidql10");
 	}
